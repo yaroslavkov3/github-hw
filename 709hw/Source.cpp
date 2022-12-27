0f97238f44d628722d96216593ce5dd732efcbbe
user <user@WIN-S70G3FQUQNP>
2022-12-27 20:48:31 +0200

    Добавьте файлы проекта.

---
+++ 709hw/Source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Point
+{
+	int x;
+	int y;
+public:
+	Point()
+	{
+		x = y = 2;
+	}
+	Point(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+	void Print()
+	{
+		cout <<"X"<< x << "\t" <<"Y" << y << endl;
+	}
+};
+int main()
+{
+	Point obj2;
+	obj2.Print();
+}
